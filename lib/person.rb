0b9ef5353e5d6d10e1cd8a84c7ff2df548fc032d
Jose Soto <jsoto7087@gmail.com>
2021-02-10 00:19:42 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,46 @@
 # your code goes here
+
+
+class Person
+
+    attr_accessor :bank_account, :happiness
+
+    def initialize(name, bank_account=25, happiness=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+       
+        if happiness > 10
+  		 happiness = 10
+        else
+         @happiness = happiness
+        end
+    end
+
+    def name 
+        @name
+    end 
+
+
+ 
+    def bank_account
+        @bank_account
+        
+    end 
+
+    def happiness
+        @happiness
+    
+  
+    end
+
+
+end 
+
+
+
+# if happiness is greater than 10
+  			#happiness equals 10
+ 		#else
+        #  @happiness = happiness
+         #end
